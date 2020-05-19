6e09d313d405d40c1eb5a22f72a05232228fe990
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-19 11:38:36 -0500

    routing set up for parkareas ViewSet in admin app's urls.py

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -17,8 +17,10 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from kennywoodapi.models import *
+from kennywoodapi.views import ParkAreas
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'parkareas', ParkAreas, 'parkarea')
 
 urlpatterns = [
     path('', include(router.urls)),
