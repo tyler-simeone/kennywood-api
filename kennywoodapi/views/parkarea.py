054453535be28c04712061490a7481d748c30ce5
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-19 14:59:23 -0500

    re-adding parkarea ViewSet...

--- kennywoodapi/views/parkarea.py
+++ kennywoodapi/views/parkarea.py
@@ -1,4 +1,4 @@
-"""View module for handling requests about park areas"""
+"""Park Areas for Kennywood Amusement Park"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
@@ -11,7 +11,7 @@ class ParkAreaSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for park areas
 
     Arguments:
-        serializers.HyperlinkedModelSerializer
+        serializers
     """
     class Meta:
         model = ParkArea
@@ -25,6 +25,21 @@ class ParkAreaSerializer(serializers.HyperlinkedModelSerializer):
 class ParkAreas(ViewSet):
     """Park Areas for Kennywood Amusement Park"""
 
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized ParkArea instance
+        """
+        newarea = ParkArea()
+        newarea.name = request.data["name"]
+        newarea.theme = request.data["theme"]
+        newarea.save()
+
+        serializer = ParkAreaSerializer(newarea, context={'request': request})
+
+        return Response(serializer.data)
+
     def retrieve(self, request, pk=None):
         """Handle GET requests for single park area
 
@@ -37,7 +52,38 @@ class ParkAreas(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
-            
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a park area
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        area = ParkArea.objects.get(pk=pk)
+        area.name = request.data["name"]
+        area.theme = request.data["theme"]
+        area.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single park area
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            area = ParkArea.objects.get(pk=pk)
+            area.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except ParkArea.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def list(self, request):
         """Handle GET requests to park areas resource
 
@@ -46,8 +92,5 @@ class ParkAreas(ViewSet):
         """
         areas = ParkArea.objects.all()
         serializer = ParkAreaSerializer(
-            areas,
-            many=True,
-            context={'request': request}
-        )
+            areas, many=True, context={'request': request})
         return Response(serializer.data)
