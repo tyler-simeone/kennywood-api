4a42acf04ba3c46808e8d8215d9c07b2781262cb
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-20 12:19:18 -0500

    imported authentication views into view package and added both to admin app's url patterns

--- kennywoodapi/views/__init__.py
+++ kennywoodapi/views/__init__.py
@@ -2,3 +2,5 @@ from .parkarea import ParkAreas
 from .itinerary import Itineraries
 from .customer import Customers
 from .attraction import Attractions
+from .register import register_user
+from .register import login_user
